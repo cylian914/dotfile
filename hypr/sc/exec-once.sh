3cf5dd009bff42c99ff16bdfd44a9b6091968bac
cylian charbonnier <cylian.charbonnier1@gmail.com>
2023-09-04 00:05:53 +0200

    add hypr

---
+++ hypr/sc/exec-once.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+hyprpaper & 
+discord-canary &
+variety &
+/usr/lib/polkit-kde-authentication-agent-1 &
+dunst &
+udiskie &
+spotify &
+
